72bca5e570c2a1143c014053ca7e4f536bc46d44
Ahmadillo20007 <ahmadilloqubijonov@gmail.com>
2023-01-29 21:45:49 +0500

    xe

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const toggle = document.querySelector(".toggle");
+const color = document.querySelector(".color");
+const body = document.querySelector("body");
+
+toggle.addEventListener("click", () => {
+    
+toggle.classList.toggle('active')
+color.classList.toggle('active')
+
+
+
+  
+});
